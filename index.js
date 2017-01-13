ccd62c5c735e5b07e5f4842d70808f445bcb0e87
Brenton Cozby <brenton.cozby@gmail.com>
2017-01-13 10:55:42 -0500

    add timestamp GET handler

--- index.js
+++ index.js
@@ -9,12 +9,23 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+app.get('/:timestamp', (req, res) => {
+    var date = new Date(req.params.timestamp);
+
+    if (isNaN(date.valueOf())) {
+        date = new Date(+req.params.timestamp * 1000);
+    }
+
+    res.end(JSON.stringify({
+        unix: date.getTime() / 1000,
+        natural: date.toLocaleDateString()
+    }));
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
-
-
